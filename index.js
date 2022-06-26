cff0d448f0e0768d0b1ff131e70079096a5dc55f
Vicenzoo <108063917+plugselect01@users.noreply.github.com>
2022-06-26 17:48:51 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const chalk = require('chalk');
+const rs = require('readline-sync');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GoStumble = (auth) => new Promise((resolve, reject) => {
+
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    method: 'GET',
+    headers: {
+      'authorization': auth
+    }
+  })
+    .then(res => res.text())
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      reject(err);
+    });
+
+});
+
+(async () => {
+
+  console.log(chalkGreen(`
+───────────▄▄▄▄▄▄▄▄▄───────────
+░██████╗░█████╗░███████╗███████╗
+██╔════╝██╔══██╗██╔════╝██╔════╝
+╚█████╗░███████║█████╗░░█████╗░░
+░╚═══██╗██╔══██║██╔══╝░░██╔══╝░░
+██████╔╝██║░░██║██║░░░░░███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚══════╝
+By : ${('VICENZO')}
+`));
+
+  const auth = rs.question(chalkYellow('Masukkan Auth Token Anda : ');
+  console.log('');
+
+  while (true) {
+
+    const result = await GoStumble(auth);
+    if (!result) {
+
+      console.log(chalkGreen(`\r[ ${moment().format('HH:mm:ss')} ] Auth Eror !`));
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+      const crown = data.User.Crowns;
+
+      console.log(chalkWhite(`\r
+-  [${moment().format('HH:mm:ss')}]  -
+>  ${(`Negara By Vicenzo : ${country}`)}
+>  ${(`Nama By Vicenzo : ${username}`)}  
+>  ${(`Piala By Vicenzo : ${trophy}`)}  
+>  ${(`Mahkota By Vicenzo : ${crown}`)}
+>  ${(`Status : Success !`)}`));
+      await sleep(3000);
+
+    } else if (result == 'BANNED') {
+      console.log(chalk.bgRed(`Mampus Banned Makanya jangan brutal`));
+     break;
+    }
+  }
+
+
+})();
